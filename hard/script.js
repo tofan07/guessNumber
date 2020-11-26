31fe7db1c561d8e03905a21c3243d8c1d037650b
tofan07 <tofan0797@gmail.com>
2020-11-26 19:45:35 +0300

    Refact hard/script.js

--- hard/script.js
+++ hard/script.js
@@ -8,54 +8,61 @@ let isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-function getUserNum(num, i) {
-  return function testNum() {
-    if (i === 1) {
-      if (confirm('Попытки закончились, хотите сыграть еще?')) {
-        iter = i + 9;
-        start();
-      } else {
-        alert ('Игра окончена!');
-        return;
-      }
-    } else {
-        if (num === null) {
-          alert ('Игра окончена!');
-          return;
-        } else if (!isNumber(num)) {
-          num = prompt('Введите число!');
-          testNum();
-        } else {
-          iter = i - 1;
-          switch (true){
-            case num < targetNum:
-              alert(`Загаданное число больше! \nОсталось попыток: ${iter}`);
-              start();
-              break;
-            case num > targetNum:
-              alert(`Загаданное число меньше! \nОсталось попыток: ${iter}`);
-              start();
-              break;
-            case num === targetNum:
-              if (confirm(`Поздравляю, Вы угадали число: ${targetNum} !!!\nХотели бы сыграть еще?`)) {
-                i = 1;
-                iter = i + 9;
-                start();
-              } else {
-                alert ('Игра окончена!');
-                return;
-              }
-          }
-        }
-      }
-  };
-}
-
 function start() {
   userNum = prompt('Угадай число от 1 до 100');
   const guessNumber = getUserNum(userNum, iter);
   guessNumber();
   console.dir(guessNumber);
+}
+
+function restart() {
+  let i = iter;
+  switch (confirm('Попытки закончились, хотите сыграть еще?')) {
+    case true:
+      iter = i + 9;
+      start();
+      break;
+    case false:
+      alert ('Игра окончена!');
+      return;
+  }
+}
+
+function getUserNum(num, i) {
+  return function numCheck() {
+    if (i === 1) {
+      restart();
+    } else {
+      if (num === null) {
+        alert ('Игра окончена!');
+        return;
+        } else if (!isNumber(num)) {
+          num = prompt('Введите число!');
+          numCheck();
+          } else {
+            iter = i - 1;
+            switch (true) {
+              case num < targetNum:
+                alert(`Загаданное число больше! \nОсталось попыток: ${iter}`);
+                start();
+                break;
+              case num > targetNum:
+                alert(`Загаданное число меньше! \nОсталось попыток: ${iter}`);
+                start();
+                break;
+              case num === targetNum:
+                if (confirm(`Поздравляю, Вы угадали число: ${targetNum} !!!\nХотели бы сыграть еще?`)) {
+                  i = 1;
+                  iter = i + 9;
+                  start();
+                } else {
+                  alert ('Игра окончена!');
+                  return;
+                }
+            }
+          }
+    }
+  };
 }
 
 start();
