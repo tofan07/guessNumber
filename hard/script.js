79d201679ab61c83bcdf086c97347fa0fb7edda5
tofan07 <tofan0797@gmail.com>
2020-11-26 20:23:22 +0300

    Refact hard/script.js

--- hard/script.js
+++ hard/script.js
@@ -8,7 +8,7 @@ let isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-function start() {
+function init() {
   userNum = prompt('Угадай число от 1 до 100');
   const guessNumber = getUserNum(userNum, iter);
   guessNumber();
@@ -19,8 +19,8 @@ function restart() {
   let i = iter;
   switch (confirm('Попытки закончились, хотите сыграть еще?')) {
     case true:
-      iter = i + 9;
-      start();
+      i += 9;
+      init();
       break;
     case false:
       alert ('Игра окончена!');
@@ -28,8 +28,33 @@ function restart() {
   }
 }
 
+function matchCheck() {
+  let num = userNum;
+  let i = iter;
+  iter = i - 1;
+  switch (true) {
+    case num < targetNum:
+      alert(`Загаданное число больше! \nОсталось попыток: ${iter}`);
+      init();
+      break;
+    case num > targetNum:
+      alert(`Загаданное число меньше! \nОсталось попыток: ${iter}`);
+      init();
+      break;
+    case num === targetNum:
+      if (confirm(`Поздравляю, Вы угадали число: ${targetNum} !!!\nХотели бы сыграть еще?`)) {
+        i = 1;
+        iter = i + 9;
+        init();
+        } else {
+          alert ('Игра окончена!');
+          return;
+      }
+  }
+}
+
 function getUserNum(num, i) {
-  return function numCheck() {
+  return function start() {
     if (i === 1) {
       restart();
     } else {
@@ -38,31 +63,13 @@ function getUserNum(num, i) {
         return;
         } else if (!isNumber(num)) {
           num = prompt('Введите число!');
-          numCheck();
+          start();
           } else {
-            iter = i - 1;
-            switch (true) {
-              case num < targetNum:
-                alert(`Загаданное число больше! \nОсталось попыток: ${iter}`);
-                start();
-                break;
-              case num > targetNum:
-                alert(`Загаданное число меньше! \nОсталось попыток: ${iter}`);
-                start();
-                break;
-              case num === targetNum:
-                if (confirm(`Поздравляю, Вы угадали число: ${targetNum} !!!\nХотели бы сыграть еще?`)) {
-                  i = 1;
-                  iter = i + 9;
-                  start();
-                } else {
-                  alert ('Игра окончена!');
-                  return;
-                }
-            }
+            matchCheck();
           }
     }
   };
 }
 
-start();
+
+init();
